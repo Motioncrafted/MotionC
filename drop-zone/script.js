fcf07d6661f9554b545106396229947ca04e8e74
Motioncrafted <kevanne@gmail.com>
2026-07-09 12:09:24 -0600

    Update script.js

--- drop-zone/script.js
+++ drop-zone/script.js
@@ -1,14 +1,26 @@
 const sprayButton = document.getElementById("sprayButton");
-const sprayMist = document.getElementById("sprayMist");
+const imageStage = document.querySelector(".image-stage");
 
 sprayButton.addEventListener("click", () => {
-
     console.log("SPRAY IT clicked!");
 
-    sprayMist.style.opacity = "1";
+    for (let i = 0; i < 40; i++) {
+        const particle = document.createElement("span");
+        particle.className = "spray-particle";
 
-    setTimeout(() => {
-        sprayMist.style.opacity = "0";
-    }, 1000);
+        particle.style.left = "69%";
+        particle.style.top = "75%";
 
+        const x = -80 - Math.random() * 90;
+        const y = -40 + Math.random() * 80;
+
+        particle.style.setProperty("--x", `${x}px`);
+        particle.style.setProperty("--y", `${y}px`);
+
+        imageStage.appendChild(particle);
+
+        setTimeout(() => {
+            particle.remove();
+        }, 650);
+    }
 });
