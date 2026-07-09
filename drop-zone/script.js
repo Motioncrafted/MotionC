e0831de14da3debc60421a43d12cf754082ea0ff
Motioncrafted <kevanne@gmail.com>
2026-07-09 11:52:38 -0600

    Update script.js

--- drop-zone/script.js
+++ drop-zone/script.js
@@ -1,3 +1,14 @@
-document.getElementById("sprayButton").addEventListener("click", () => {
+const sprayButton = document.getElementById("sprayButton");
+const sprayMist = document.getElementById("sprayMist");
+
+sprayButton.addEventListener("click", () => {
+
     console.log("SPRAY IT clicked!");
+
+    sprayMist.style.opacity = "1";
+
+    setTimeout(() => {
+        sprayMist.style.opacity = "0";
+    }, 250);
+
 });
