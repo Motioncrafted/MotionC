0ccb5d221ec79685b576f5fbde7e09c7d3c21fdd
Motioncrafted <kevanne@gmail.com>
2026-07-09 12:35:53 -0600

    Update script.js

--- drop-zone/script.js
+++ drop-zone/script.js
@@ -8,7 +8,7 @@ function createSprayBurst() {
         const particle = document.createElement("span");
         particle.className = "spray-particle";
 
-       particle.style.left = "73%";
+       particle.style.left = "76%";
         particle.style.top = "61%";
 
         const x = -80 - Math.random() * 90;
