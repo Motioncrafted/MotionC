cb10a072d8e39a40e72e7033708486a587034667
Motioncrafted <kevanne@gmail.com>
2026-07-14 19:27:00 -0600

    Update script.js

--- dashboard/script.js
+++ dashboard/script.js
@@ -4,19 +4,18 @@ const overlay = document.querySelector('.drawer-overlay');
 const openBtn = document.getElementById('kscore-button');
 const closeBtn = document.querySelector('.drawer-close-btn');
 
-if (openBtn && drawer && overlay && closeBtn) {
-    openBtn.addEventListener('click', () => {
-        drawer.classList.add('open');
-        overlay.classList.add('open');
-    });
+openBtn.addEventListener('click', () => {
+    drawer.classList.add('open');
+    overlay.classList.add('open');
+});
 
-    closeBtn.addEventListener('click', () => {
-        drawer.classList.remove('open');
-        overlay.classList.remove('open');
-    });
+closeBtn.addEventListener('click', () => {
+    drawer.classList.remove('open');
+    overlay.classList.remove('open');
+});
 
-    overlay.addEventListener('click', () => {
-        drawer.classList.remove('open');
-        overlay.classList.remove('open');
-    });
-}
+overlay.addEventListener('click', () => {
+    drawer.classList.remove('open');
+    overlay.classList.remove('open');
+});
+
